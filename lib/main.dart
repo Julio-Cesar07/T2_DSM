c76e81b3bb36875c693a7e9ec08f03255e26d9db
Julio-Cesar07 <julio.oliveira@catijr.com.br>
2022-09-28 22:54:45 -0300

    conectando no firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
+import 'package:firedart/firedart.dart';
 import 'package:flutter/material.dart';
 import 'package:t2_dsw/pages/createaccount.page.dart';
 import 'package:t2_dsw/pages/home.service.dart';
@@ -9,11 +10,26 @@ import 'package:t2_dsw/pages/rememberpass.page.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'generated/l10n.dart';
 
-void main() => runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-class MyApp extends StatelessWidget {
+const projectId = 't2-dsm-952e7';
+
+void main() async{
+  Firestore.initialize(projectId);
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  State<MyApp> createState() => _MyApp();  
+}
+
+class _MyApp extends State<MyApp> {
+
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
