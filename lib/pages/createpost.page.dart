1c7d5ef4cee2af47144ae6e52c5eae132d33842e
Julio-Cesar07 <julio.oliveira@catijr.com.br>
2022-09-28 23:22:54 -0300

    create post

--- lib/pages/createpost.page.dart
+++ lib/pages/createpost.page.dart
@@ -1,3 +1,4 @@
+import 'package:firedart/firedart.dart';
 import 'package:flutter/material.dart';
 import 'package:t2_dsw/const/colors.dart';
 import 'package:t2_dsw/generated/l10n.dart';
@@ -10,7 +11,13 @@ class CreatePost extends StatefulWidget {
 }
 
 class _CreatePostState extends State<CreatePost> {
+  CollectionReference postCollection = Firestore.instance.collection('post');
+
+  DateTime data = DateTime.now();
+
   final TextEditingController controllerPost = TextEditingController();
+  final TextEditingController controllerRank = TextEditingController();
+  final TextEditingController controllerVagancies = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,11 +34,35 @@ class _CreatePostState extends State<CreatePost> {
                     decoration: InputDecoration(
                         hintText: S.current.writePost,
                         hintStyle: TextStyle(color: whiteColor))),
+                Padding(padding: const EdgeInsets.all(16.0)),        
+                TextFormField(
+                    controller: controllerRank,
+                    style: TextStyle(
+                      color: whiteColor,
+                    ),
+                    decoration: InputDecoration(
+                        hintText: S.current.writePost,
+                        hintStyle: TextStyle(color: whiteColor))),
+                Padding(padding: const EdgeInsets.all(16.0)),
+                TextFormField(
+                    controller: controllerVagancies,
+                    style: TextStyle(
+                      color: whiteColor,
+                    ),
+                    decoration: InputDecoration(
+                        hintText: S.current.writePost,
+                        hintStyle: TextStyle(color: whiteColor))),
                 Spacer(),
                 ElevatedButton(
                   child: Text(S.current.post),
                   onPressed: () {
-                    print(controllerPost.text);
+                    postCollection.add({
+                      'text': controllerPost.text,
+                      'rank': controllerRank.text,
+                      'vagancies': controllerVagancies.text,
+                      'date': data,
+                    });
+                    Navigator.pushNamed(context, '/home');
                   },
                 ),
                 Padding(padding: const EdgeInsets.all(24.0)),
